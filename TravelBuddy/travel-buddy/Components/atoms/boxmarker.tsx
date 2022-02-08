68b398d383efaa493c21cdf038460314d9e6f2a9
Mason Powell <slicace1234@gmail.com>
2022-02-08 12:43:00 -0800

    styled cards more and got hover to work

--- TravelBuddy/travel-buddy/Components/atoms/boxmarker.tsx
+++ TravelBuddy/travel-buddy/Components/atoms/boxmarker.tsx
@@ -7,8 +7,13 @@ import {
   Stack,
   Image,
   Flex,
+  Grid,
+  GridItem,
+  VStack,
 } from '@chakra-ui/react';
 import { GoogleMap, Marker } from '@react-google-maps/api';
+
+import { BsStar, BsStarFill, BsStarHalf } from 'react-icons/bs';
 
 const data = {
   isNew: true,
@@ -20,25 +25,80 @@ const data = {
   numReviews: 34,
 };
 
-export default function BoxMarker() {
+interface MarkerProps {
+  name: string;
+  image: string;
+  rating: number;
+  numReviews: number;
+}
+
+interface RatingProps {
+  rating: number;
+  numReviews: number;
+}
+
+function Rating({ rating, numReviews }: RatingProps) {
+  return (
+    <Box d="flex" alignItems="center">
+      {Array(5)
+        .fill('')
+        .map((_, i) => {
+          const roundedRating = Math.round(rating * 2) / 2;
+          if (roundedRating - i >= 1) {
+            return (
+              <BsStarFill color='gold'
+                key={i}
+                style={{ marginLeft: '1' }}
+              />
+            );
+          }
+          if (roundedRating - i === 0.5) {
+            return <BsStarHalf color='gold' key={i} style={{ marginLeft: '1' }} />;
+          }
+          return <BsStar color='gold' key={i} style={{ marginLeft: '1' }} />;
+        })}
+    </Box>
+  );
+}
+
+export default function BoxMarker({name, image, rating, numReviews}: MarkerProps) {
+
+
+
+
+
   return (
       <Flex
-      w={100}
-      h={50}
-      backgroundColor={'white'}
+      position={'absolute'}
+      transform={'translate(-50%, -50%)'}
+      zIndex={1}
+      _hover={{zIndex: 2}}
+      boxShadow={'lg'}
+      cursor={'pointer'}
+      w={120}
+      h={100}
+      backgroundColor='gray'
       direction={'column'}
-      alignContent={'flex-start'}
-
+      justifyContent={'center'}
+      alignItems={'center'}
       >
-        <Text w>
-          testing the text here
-        </Text>
-        <Image src={data.imageURL} width={'inhert'}>
 
-        </Image>
-        <Box>
-          ratings go here
-        </Box>
+          <Text>
+            {name}
+          </Text>
+
+
+        <Image
+        marginTop={1}
+        marginBottom={1}
+        width={'90%'}
+        h={'60%'}
+        src={image}
+        alt={'test'}/>
+
+
+           <Rating rating={rating} numReviews={numReviews}/>
+
       </Flex>
   );
 }
